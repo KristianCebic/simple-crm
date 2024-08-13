182a3455c9a83a802dca088afe574a6e1c7e010b
KristianCebic <kristian.cebic13@gmail.com>
2024-08-13 15:01:15 +0200

    last version of the project

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    RouterModule
+  ],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss',
+})
+export class DialogAddUserComponent {
+  user = new User();
+  birthDate!: Date;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('current user is ', this.user);
+  }
+}
